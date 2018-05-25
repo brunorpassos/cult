54228cdf09cf6de5b65a074e12811040ef09c1e0
Bruno Passos <bruno.passos@creditas.com.br>
2018-05-25 18:44:10 -0300

    Several components

---
+++ app/helpers/book-property-type.js
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+
+export function bookPropertyType(params/*, hash*/) {
+  return params;
+}
+
+export default helper(bookPropertyType);
--- mirage/config.js
+++ mirage/config.js
@@ -28,9 +28,35 @@ export default function() {
           owner_email: 'bruno.passos@creditas.com.br',
           image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
         }
-      }, {
+      },{
         type: 'books',
         id: '0003',
+        attributes: {
+          id: '0002',
+          author: 'Karl Marx',
+          title: 'O Capital',
+          publisher: '',
+          ISBN: '978-3-16-148410-0',
+          owner: 'Bruno Passos',
+          owner_email: 'bruno.passos@creditas.com.br',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+        }
+      }, {
+        type: 'books',
+        id: '0004',
+        attributes: {
+          id: '0002',
+          author: 'Karl Marx',
+          title: 'O Capital',
+          publisher: '',
+          ISBN: '978-3-16-148410-0',
+          owner: 'Bruno Passos',
+          owner_email: 'bruno.passos@creditas.com.br',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+        }
+      }, {
+        type: 'books',
+        id: '0005',
         attributes: {
           id: '0003',
           author: 'Design Patterns',
